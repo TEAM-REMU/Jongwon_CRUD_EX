b9cc35ae639d6ad5a1ad8c10a300f55ccdd927f2
JongwonYoun <jeffyoun@naver.com>
2020-07-14 20:25:03 +0900

    Posts 불러와서 Home에서 보여줄 수 있도록 구현

--- blogProject/blog/models.py
+++ blogProject/blog/models.py
@@ -3,18 +3,23 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+    # 작성자
     author = models.CharField( max_length=10)
+    # 제목
     title = models.CharField(max_length=50)
-
+    # 본문
     body = models.TextField()
-
+    # 작성일
     created_date = models.DateTimeField(default=timezone.now)
-
+    # 수정일
     edited_date = models.DateTimeField(default=timezone.now)
 
+    # 포스트 작성 함수
+    # 글 수정시 수정 시간 자동으로 업데이트 하도록 하는 함수
     def write(self):
         self.edited_date = timezone.now()
         self.save()
 
+    # 포스트를 나타내는 문자열에 제목 반환
     def __str__(self):
         return self.title
--- blogProject/blog/views.py
+++ blogProject/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+# 메인 페이지의 포스트를 최신순으로 불러와 템플릿에 전달
 def home(request):
-    return render(request, 'home.html')
+
+    posts = Post.objects.all().order_by('-created_date')
+
+    return render(request, 'home.html', {'posts' : posts})
