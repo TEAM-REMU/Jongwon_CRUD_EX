cb26c884f33e82e98e6f9344419cb4598bb7b1c1
JongwonYoun <jeffyoun@naver.com>
2020-07-14 20:27:33 +0900

    views.py home 주석 추가

--- blogProject/blog/views.py
+++ blogProject/blog/views.py
@@ -3,7 +3,8 @@ from .models import Post
 
 # 메인 페이지의 포스트를 최신순으로 불러와 템플릿에 전달
 def home(request):
-
+    # 포스트를 시간순으로 불러온다
     posts = Post.objects.all().order_by('-created_date')
 
+    # 그리고 home.html에 넘겨준다
     return render(request, 'home.html', {'posts' : posts})
